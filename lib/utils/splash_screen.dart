fd29ae1f1df033273a2939473e4d3fda62c75b70
Irfan Maulana <irfan.resimen2018@gmail.com>
2022-06-07 16:14:01 +0700

    Initial Commit

---
+++ lib/utils/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
+}
